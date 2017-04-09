b82bad98d5dc0aad0817944a4a93f32373fd1bba
gaolei <470579032@qq.com>
2017-04-09 20:31:43 +0800

    VR图片效果

---
+++ app/src/main/java/com/gl/vrdemo/MainActivity.java
@@ -0,0 +1,81 @@
+package com.gl.vrdemo;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.vr.sdk.widgets.common.VrWidgetView;
+import com.google.vr.sdk.widgets.pano.VrPanoramaView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    private VrPanoramaView vr_panorama;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //控件初始化
+        vr_panorama = (VrPanoramaView) findViewById(R.id.vr_panorama);
+        //切换VR的模式
+        vr_panorama.setDisplayMode(VrWidgetView.DisplayMode.FULLSCREEN_STEREO);
+        //使用自定义的AsyncTask,播放VR效果
+        new vrasty().execute();
+
+    }
+    //创建一个异步，由于VR资源数据量大,获取需要时间,故把加载图片放到子线程中进行,主线程来显示图片,故可以使用一个异步线程AsyncTask或EventBus来处理.
+    private class vrasty extends AsyncTask<String,Void,Bitmap>{
+        //该方法在子线程运行,从本地文件中把资源加载到内存中.
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try {
+                //转成字节输入流
+                InputStream open = getAssets().open("andes.jpg");
+                //把字节输入流转换成Bitmap对象
+                Bitmap bitmap = BitmapFactory.decodeStream(open);
+                return bitmap;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        //该方法在主线程运行
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            //创建bVrPanoramaView.Options,去决定显示VR是普通效果,还是立体效果
+            VrPanoramaView.Options options = new VrPanoramaView.Options();
+
+            //TYPE_STEREO_OVER_UNDER立体效果:图片的上半部分放在左眼显示,下半部分放在右眼显示     TYPE_MONO:普通效果
+            options.inputType=VrPanoramaView.Options.TYPE_STEREO_OVER_UNDER;
+
+            //使用VR控件对象,显示效果  参数:1.Bitmap对象      2.VrPanoramaView.Options对象,决定显示的效果
+            vr_panorama.loadImageFromBitmap(bitmap,options);
+        }
+    }
+
+    //失去焦点的时候调用
+    @Override
+    protected void onPause() {
+        super.onPause();
+        vr_panorama.pauseRendering();
+    }
+    //再次获取焦点的时候调用
+    @Override
+    protected void onResume() {
+        super.onResume();
+        vr_panorama.resumeRendering();
+    }
+    //销毁的时候调用
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        vr_panorama.shutdown();
+
+    }
+}
